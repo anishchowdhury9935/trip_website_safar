9103c7a033e3f01574d74e838cbd9950e28f636f
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-12-03 17:40:55 +0530

    save

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
+// import Image from 'next/image';
+import React from 'react';
 import Link from 'next/link';
 import './styles/Navbar.css';
 
--- src/app/components/error/404.tsx
+++ src/app/components/error/404.tsx
@@ -4,7 +4,7 @@ export default function NotFoundError() {
     return (
         <div>
             <h1>404 - Page Not Found</h1>
-            <p>The page you're looking for does not exist.</p>
+            <p>The page you&apos;re looking for does not exist.</p>
         </div>
     );
 }
