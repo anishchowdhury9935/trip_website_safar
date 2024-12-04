e8464911fefe2143d7574380d55ade2eb3df0971
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-12-04 16:49:58 +0530

    update : style import remove from app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import "../app/css/globals.css";
-import styles from "./page.module.css"
+// import styles from "./page.module.css"
 
 export default function root() {
   location.pathname = "/components/home";
